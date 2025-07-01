712881166af117051a42023f6a6e48bf3342ef99
Ravenna <ravennasantos2016@gmail.com>
2025-07-01 12:13:06 -0300

    Tarefa #2 Testes de Integração

--- src/test/java/br/com/gerenciamento/controller/AlunoControllerTest.java
+++ src/test/java/br/com/gerenciamento/controller/AlunoControllerTest.java
@@ -1,10 +1,100 @@
 package br.com.gerenciamento.controller;
 
+import br.com.gerenciamento.model.Aluno;
+import br.com.gerenciamento.enums.Curso;
+import br.com.gerenciamento.enums.Status;
+import br.com.gerenciamento.enums.Turno;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import br.com.gerenciamento.repository.AlunoRepository;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class AlunoControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    @Before
+    public void beforeEach() {
+        alunoRepository.deleteAll();
+    }
+
+    @After
+    public void afterEach() {
+        alunoRepository.deleteAll();
+    }
+
+    @Test
+    public void deveCadastrarAluno() throws Exception {
+        mockMvc.perform(post("/InsertAlunos")
+                .param("nome", "Aluno Create")
+                .param("matricula", "10001")
+                .param("curso", "INFORMATICA")
+                .param("status", "ATIVO")
+                .param("turno", "MATUTINO"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/alunos-adicionados"));
+    }
+    @Test
+    public void deveExibirPaginaFiltroAlunos() throws Exception {
+        mockMvc.perform(get("/filtro-alunos"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("Aluno/filtroAlunos"));
+    }
+
+    @Test
+    public void deveAtualizarAluno() throws Exception {
+        Aluno aluno = new Aluno();
+        aluno.setNome("Aluno Update");
+        aluno.setMatricula("10002");
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setTurno(Turno.NOTURNO);
+        Aluno salvo = alunoRepository.save(aluno);
+
+        mockMvc.perform(post("/editar")
+                .param("id", salvo.getId().toString())
+                .param("nome", "Aluno Atualizado")
+                .param("matricula", "10002")
+                .param("curso", "ADMINISTRACAO")
+                .param("status", "ATIVO")
+                .param("turno", "NOTURNO"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/alunos-adicionados"));
+    }
+
+    @Test
+    public void deveDeletarAluno() throws Exception {
+        Aluno aluno = new Aluno();
+        aluno.setNome("Aluno Delete");
+        aluno.setMatricula("10003");
+        aluno.setCurso(Curso.DIREITO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setTurno(Turno.MATUTINO);
+        Aluno salvo = alunoRepository.save(aluno);
+
+        mockMvc.perform(get("/remover/" + salvo.getId()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/alunos-adicionados"));
+    }
 }
--- src/test/java/br/com/gerenciamento/controller/UsuarioControllerTest.java
+++ src/test/java/br/com/gerenciamento/controller/UsuarioControllerTest.java
@@ -1,10 +1,52 @@
 package br.com.gerenciamento.controller;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class UsuarioControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void deveExibirPaginaLogin() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login/login"));
+    }
+
+    @Test
+    public void deveExibirPaginaCadastro() throws Exception {
+        mockMvc.perform(get("/cadastro"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login/cadastro"));
+    }
+
+    @Test
+    public void deveRedirecionarAoSalvarUsuario() throws Exception {
+        mockMvc.perform(post("/salvarUsuario")
+                .param("user", "usuarioTeste")
+                .param("senha", "senha123")
+                .param("email", "teste@email.com"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
+    }
+
+    @Test
+    public void deveFazerLogoutERedirecionarParaLogin() throws Exception {
+    mockMvc.perform(post("/logout"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("login/login"));
+}
 }
